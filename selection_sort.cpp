9d9d13ac70e0308f81f853536af407675f558489
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2025-01-18 10:04:09 +0530

    Path generalized

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,58 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// selection sort function
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        swap(arr[i], arr[minIndex]);
-    }
-}
-
-
-// generate random array of size n
-void generateRandomArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand() % 100;
-    }
-}
-
-// print array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-
-int main() {
-    // Seed the random number generator
-    srand(time(0));
-
-    // Generate a random array of size 10
-    int n = 100000;
-    int arr[n];
-    generateRandomArray(arr, n);
-
-    // Print the unsorted array
-    // cout << "Unsorted array: ";
-    // printArray(arr, n);
-
-    // Sort the array using selection sort
-    selectionSort(arr, n);
-
-    // Print the sorted array
-    // cout << "Sorted array: ";
-    // printArray(arr, n);
-
-    return 0;
-}#include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -103,8 +49,8 @@ void printList(Node* node) {
 int main() {
     Node* head = nullptr;
     std::srand(std::time(0));
-    for (int i = 0; i < 100; ++i) {
-        append(&head, std::rand() % 1000);
+    for (int i = 0; i < 10; ++i) {
+        append(&head, std::rand() % 10000);
     }
 
     std::cout << "Unsorted list: ";
